02e2e8b9fc46caba7d65597e370f567477eda88a
leminhduy1912 <minhduyle081003@gmail.com>
2024-06-29 00:55:08 +0700

    first commit

---
+++ src/components/Result.jsx
@@ -0,0 +1,21 @@
+import "./Result.css";
+
+const Result = (props) => {
+  console.log(props);
+  const { number } = props;
+  const digits = number.toString().split("");
+  return (
+    <>
+      <div className="main">
+        <div className="odometer">
+          {digits.map((digit, index) => (
+            <span key={index}>{digit}</span>
+          ))}
+          <span className="unit">km</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Result;
