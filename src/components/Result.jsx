dade193c2237b4c6358e1c779d436109d46d8d33
leminhduy1912 <minhduyle081003@gmail.com>
2024-07-28 13:02:49 +0700

    custom font

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,17 +1,62 @@
 import "./Result.css";
-
-const Result = (props) => {
-  console.log(props);
-  const { number } = props;
-
+import { FaSquareFull } from "react-icons/fa";
+const Result = ({ number }) => {
+  let digits = number.split("");
   return (
     <>
       <div className="main">
         <div className="odometer">
-          {/* {digits.map((digit, index) => (
-            <span key={index}>{digit}</span>
-          ))} */}
-          {number}
+          {digits.map((digit, index) => {
+            if (digit == 3) {
+              return (
+                <div key={index}>
+                  <span
+                    style={{
+                      position: "relative",
+                    }}
+                  >
+                    {digit}
+                    <FaSquareFull
+                      color="#1c2125"
+                      style={{
+                        width: "0.1rem",
+                        height: "0.1rem",
+                        position: "absolute",
+                        top: "0.72rem",
+                        left: "0.12rem",
+                      }}
+                    />
+                  </span>
+                </div>
+              );
+            } else if (digit == 4) {
+              return (
+                <div key={index}>
+                  <span
+                    style={{
+                      position: "relative",
+                    }}
+                  >
+                    {digit}
+                    <FaSquareFull
+                      style={{
+                        backgroundColor: "red",
+                        width: "0.02rem",
+                        minHeight: "0.5rem",
+                        maxHeight: "1rem",
+                        position: "absolute",
+                        top: "0.8rem",
+                        left: "0.328rem",
+                      }}
+                    />
+                  </span>
+                </div>
+              );
+            } else {
+              return <div key={index}>{digit}</div>;
+            }
+          })}
+
           <span className="unit">km</span>
         </div>
       </div>
