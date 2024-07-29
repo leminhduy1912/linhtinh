cb890299f6af1c295ee36f116d496e87f3ff7aca
leminhduy1912 <minhduyle081003@gmail.com>
2024-07-29 10:28:08 +0700

    custom font

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,25 +1,95 @@
 import "./Result.css";
+import num0 from '../assets/so0.jpg';
+import num6 from '../assets/so6.jpg';
+import num1 from '../assets/so1.jpg';
+import num9 from '../assets/so9.jpg';
+import num3 from '../assets/so3.jpg';
 import { FaSquareFull } from "react-icons/fa";
+
 const Result = ({ number }) => {
   let digits = number.split("");
   return (
     <>
       <div className="main">
         <div className="odometer">
-          {digits.map((item, index) => {
-            return (
-              <span
-                key={index}
-                style={{
-                  fontFamily: "Zain",
-                  fontSize: "16px",
-                  fontWeight: "600",
-                  fontStyle: "normal",
-                }}
-              >
-                {item}
-              </span>
-            );
+          {digits.map((digit, index) => {
+            if (digit == 0) {
+              return (
+<img src={num0} alt="" />
+              )
+             
+            }
+            if (digit == 1) {
+              return (
+<img src={num1} alt="" />
+              )
+             
+            }
+            if (digit == 3) {
+              return (
+                <img src={num3} alt="" />
+                              )
+            }
+            // if (digit == 2) {
+            //   return (
+            //     <img src={num1} alt="" />
+            //                   )
+            // }
+            // if (digit == 3) {
+            //   return (
+            //     <div key={index}>
+            //       <span
+            //         style={{
+            //           fontFamily: "Roboto Mono,monospace",
+            //           fontOpticalSizing: "auto",
+            //           fontSize: "0.6rem",
+            //           fontWeight: "bold",
+            //           fontStyle: "normal",
+            //         }}
+            //       >
+            //         {digit}
+            //       </span>
+            //     </div>
+            //   );
+            // }
+            if (digit == 6) {
+              return (
+                <img src={num6} alt="" />
+                              )
+            }
+            if (digit == 8) {
+              return (
+                <div key={index}>
+                  <span>{digit}</span>
+                </div>
+              );
+            }
+            if (digit == 9) {
+              return (
+                <img src={num9} alt="" />
+                              )
+            }
+           
+            // if (digit == 3) {
+            //   return (
+            //     <div key={index}>
+            //       <span>{digit}</span>
+            //     </div>
+            //   );
+            // }
+            // return (
+            //   <span
+            //     key={index}
+            //     style={{
+            //       fontFamily: "Zain",
+            //       fontSize: "16px",
+            //       fontWeight: "650",
+            //       fontStyle: "normal",
+            //     }}
+            //   >
+            //     {item}
+            //   </span>
+            // );
           })}
           {/* {digits.map((digit, index) => {
             if (digit == 3) {
@@ -72,7 +142,7 @@ const Result = ({ number }) => {
             }
           })} */}
 
-          <span className="unit">km</span>
+          {/* <span className="unit">km</span> */}
         </div>
       </div>
     </>
