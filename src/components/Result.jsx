191951d3cb279f2bc35cf6b993b604e55413558a
leminhduy1912 <minhduyle081003@gmail.com>
2024-07-30 12:34:59 +0700

    custom font

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,12 +1,12 @@
 import "./Result.css";
 import num0 from '../assets/so0.jpg';
-import num1 from '../assets/so1.jpg';
+import num1 from '../assets/so1test.png';
 import num2 from '../assets/so2.jpg';
 import num3 from '../assets/so3.jpg';
 import num4 from '../assets/so4.jpg';
 import num5 from '../assets/so5.jpg';
 import num6 from '../assets/so6.jpg';
-import num7 from '../assets/so7.jpg';
+import num7 from '../assets/so7test.png';
 import num8 from '../assets/so8.jpg';
 import num9 from '../assets/so9.jpg';
 import km from '../assets/km.jpg'
@@ -85,3 +85,36 @@ const Result = ({ number }) => {
 };
 
 export default Result;
+// import React from 'react'
+
+// const Result = ({ number }) => {
+//   let digits = number.split("");
+//    return (
+//      <>
+//        <div className="main">
+//          <div className="odometer">
+//            {digits.map((digit, index) => {
+//             return (
+//               <span>{digit}</span>
+//             )
+//             if (digit == 1) {
+//               console.log("1");
+              
+//               <span style={{ fontFamily: "Dongle", fontWeight: 900, fontStyle: "normal" }}>
+//               {digit}
+//             </span>
+//           }
+//               if (digit == 7) {
+//                 console.log("7");
+                
+//                 <span style={{ fontFamily: "Noto Sans KR", fontWeight: 900, fontStyle: "normal" }}>
+//                 {digit}
+//               </span>
+//             }
+//            })}
+// </div>
+//     </div>
+// </>)
+// }
+
+// export default Result
