694107b41a997d92c572428cd459909c78042095
leminhduy1912 <minhduyle081003@gmail.com>
2024-07-30 17:23:10 +0700

    custom font

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -12,110 +12,169 @@ import num9 from '../assets/so9.jpg';
 import km from '../assets/km.jpg'
 import { FaSquareFull } from "react-icons/fa";
 
-const Result = ({ number }) => {
-  let digits = number.split("");
-  return (
-    <>
-      <div className="main">
-        <div className="odometer">
-          {digits.map((digit, index) => {
-            if (digit == 0) {
-              return (
-<img src={num0} alt="" />
-              )
-             
-            }
-            if (digit == 1) {
-              return (
-                  <img src={num1} alt="" />
-              )
-             
-            }
-            if (digit == 2) {
-              return (
-                <img src={num2} alt="" />
-                              )
-            }
-            if (digit == 3) {
-              return (
-                <img src={num3} alt="" />
-                              )
-            }
-            if (digit == 4) {
-              return (
-                <img src={num4} alt="" />
-                              )
-            }
-          
-            if (digit == 5) {
-              return (
-                <img src={num5} alt="" />
-                              )
-            }
-            if (digit == 6) {
-              return (
-                <img src={num6} alt="" />
-                              )
-            }
-            if (digit == 7) {
-              return (
-                <img src={num7} alt="" />
-                              )
-            }
-            if (digit == 8) {
-              return (
-                <img src={num8} alt="" />
-                              )
-            }
-            if (digit == 9) {
-              return (
-                <img src={num9} alt="" />
-                              )
-            }
-            
-            
-          })}
-          
-          {/* <img src={km} alt="" className=" mt-2 "/> */}
-     
-         
-        </div>
-      </div>
-    </>
-  );
-};
-
-export default Result;
-// import React from 'react'
-
 // const Result = ({ number }) => {
 //   let digits = number.split("");
-//    return (
-//      <>
-//        <div className="main">
-//          <div className="odometer">
-//            {digits.map((digit, index) => {
-//             return (
-//               <span>{digit}</span>
-//             )
-//             if (digit == 1) {
-//               console.log("1");
-              
-//               <span style={{ fontFamily: "Dongle", fontWeight: 900, fontStyle: "normal" }}>
-//               {digit}
-//             </span>
-//           }
-//               if (digit == 7) {
-//                 console.log("7");
-                
-//                 <span style={{ fontFamily: "Noto Sans KR", fontWeight: 900, fontStyle: "normal" }}>
-//                 {digit}
-//               </span>
+//   return (
+//     <>
+//       <div className="main">
+//         <div className="odometer">
+//           {digits.map((digit, index) => {
+//             if (digit == 0) {
+//               return (
+// <img src={num0} alt="" />
+//               )
+             
 //             }
-//            })}
-// </div>
-//     </div>
-// </>)
-// }
+//             if (digit == 1) {
+//               return (
+//                   <img src={num1} alt="" />
+//               )
+             
+//             }
+//             if (digit == 2) {
+//               return (
+//                 <img src={num2} alt="" />
+//                               )
+//             }
+//             if (digit == 3) {
+//               return (
+//                 <img src={num3} alt="" />
+//                               )
+//             }
+//             if (digit == 4) {
+//               return (
+//                 <img src={num4} alt="" />
+//                               )
+//             }
+          
+//             if (digit == 5) {
+//               return (
+//                 <img src={num5} alt="" />
+//                               )
+//             }
+//             if (digit == 6) {
+//               return (
+//                 <img src={num6} alt="" />
+//                               )
+//             }
+//             if (digit == 7) {
+//               return (
+//                 <img src={num7} alt="" />
+//                               )
+//             }
+//             if (digit == 8) {
+//               return (
+//                 <img src={num8} alt="" />
+//                               )
+//             }
+//             if (digit == 9) {
+//               return (
+//                 <img src={num9} alt="" />
+//                               )
+//             }
+            
+            
+//           })}
+          
+//           {/* <img src={km} alt="" className=" mt-2 "/> */}
+     
+         
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
 
-// export default Result
+// export default Result;
+import React from 'react'
+
+const Result = ({ number }) => {
+  let digits = number.split("");
+   return (
+     <>
+       <div className="main">
+         <div className="odometer">
+         {digits.map((digit, index) => {
+  if (digit == 1) {
+    console.log("Dongle");
+    return (
+      <span
+      key={index}
+      style={{ backgroundColor:"black",paddingTop:"0.2rem",fontFamily: "Dongle",fontSize:"14.7px", fontWeight: 400, fontStyle: "normal" }}
+
+    >
+      {digit}
+    </span>
+     
+    );
+  }else if (digit == 0) {
+    console.log("Noto");
+    return (
+      <span
+      key={index}
+      style={{ backgroundColor:"black",paddingTop:"0.2rem",fontFamily: "Dongle",fontSize:"14.7px", fontWeight: 400, fontStyle: "normal" }}
+
+    >
+      {digit}
+    </span>
+    );
+  } else if (digit == 2) {
+    console.log("Noto");
+    return (
+      <span
+        key={index}
+        style={{ backgroundColor:"black",paddingTop:"0.2rem",fontFamily: "Dongle",fontSize:"14.7px", fontWeight: 400, fontStyle: "normal" }}
+
+      >
+        {digit}
+      </span>
+    );
+
+  } 
+  else if (digit == 3) {
+    console.log("Noto");
+    return (
+      <span
+        key={index}
+        style={{paddingTop:"0.08rem", backgroundColor:"black",fontFamily: "Lato",fontSize:"7.8px", fontWeight: 900, fontStyle: "normal" }}
+
+      >
+        {digit}
+      </span>
+    );
+
+  }  else if (digit == 7) {
+    console.log("Noto");
+    return (
+      <span
+        key={index}
+        style={{  fontFamily: "Noto Sans KR",fontSize:"9px", fontWeight: 900, fontStyle: "normal" }}
+      >
+        {digit}
+      </span>
+    );
+   
+  }  else if (digit == 8) {
+    console.log("Noto");
+    return (
+      <span
+        key={index}
+        style={{  fontFamily: "Roboto Mono",fontSize:"8.2px", fontWeight: 500, fontStyle: "normal" }}
+      >
+        {digit}
+      </span>
+    )}else {
+    return (
+      <span key={index}>
+        {digit}
+      </span>
+    );
+  }
+})}
+
+</div>
+    </div>
+</>)
+}
+
+export default Result
